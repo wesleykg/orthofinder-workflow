4b98a42c452e2a7ab2a098478f37dbbdddd2724e
wesleykg <wesley.gerelle@gmail.com>
2016-02-22 16:00:07 -0800

    added more

--- filter.py
+++ filter.py
@@ -1,7 +1,16 @@
 from Bio import SeqIO
 
+def name_trunc(record):
+    ID = record.id.split()
+    return ID
+    
 atpD_index = SeqIO.index("At-atpD_orthofinder.fna", "fasta")
+
+atpD_index.name()
 
 ids_1kp = open("ids_1kp.txt", 'r')
 wanted_species = ids_1kp.readlines()
 ids_1kp.close()
+
+#for id in atpD_index:
+    
