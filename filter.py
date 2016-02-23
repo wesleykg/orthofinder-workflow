133e41415c848513adffe9e624234c5a8a3f866a
wesleykg <wesley.gerelle@gmail.com>
2016-02-23 11:31:53 -0800

    started orthofinder data loop

--- filter.py
+++ filter.py
@@ -5,11 +5,13 @@ orthofinder_data = "At-atpD_orthofinder.fna"
 
 ##############################################################################
 
-atpD_index = SeqIO.index(orthofinder_data, "fasta")
-
 ##Produce a list of 1kp IDs
 wanted_ids = [] #Initialize the list
 with open(ids_1kp, 'r') as ids: #Open the file
     wanted_ids_temp = ids.readlines() #Read in the file
     for ID in wanted_ids_temp: 
         wanted_ids.append(ID.rstrip()) #Remove newline characters
+
+##Loop through orthofinder data and retrieve records that match wanted_ids
+
+
