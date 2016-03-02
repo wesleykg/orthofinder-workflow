ab143b23a18f822ca823cdeda2ac7d4e9bafc2a9
wesleykg <wesley.gerelle@gmail.com>
2016-03-02 15:47:39 -0800

    refactored functions; better out filename

--- filter.py
+++ filter.py
@@ -1,31 +1,37 @@
-from docopt import docopt #For command-line arguments
-from os import path #For creating output filename from input
+#from docopt import docopt #For command-line arguments
+import os #For creating output filename from input
 from Bio import SeqIO #For reading orthgroup data
 from Bio.SeqUtils.CheckSum import seguid #For identifying unique sequences
 
 ids_1kp_file = 'wanted_species.txt' #The file containing wanted 1kp IDs
-orthogroup_file = "AT4G09650_4729.fna" # The file produced by orthofinder
+orthogroup_file = "AT4G09650_4729.fna" #The file produced by orthofinder
+orthogroup_name = os.path.splitext(orthogroup_file)[0] #Drop the filename
 
 def filter_by_id(orthgroup_file, ids_1kp_file):
     '''Filter out transcripts in orthgroup data by species ID'''
     
     ##Read in list of 1kp IDs of wanted species
     wanted_ids = [] #Initialize list of IDs
+    
     with open(ids_1kp_file, 'r') as ids: #Open the file for reading
         wanted_ids_temp = ids.readlines() #Read in the file with newline chars
         for ID in wanted_ids_temp: #Loops through each ID
             wanted_ids.append(ID.rstrip()) #Remove newline chars
+    
     ##Retrieve records in orthgroup of wanted species only
+    global matching_records 
     matching_records = [] #Initialize list of records
+    
     for record in SeqIO.parse(orthogroup_file, 'fasta'): #Open the file
         for ID in wanted_ids: #Loop through each species ID
-            if ID in record.id: #The '.id' returns the names of records
+            if ID in record.id: #The '.id' method returns the names of records
                 matching_records.append(record) #Add record to list
     
     return matching_records
     
 def duplicate_remover(matching_records):
     '''Return a list of orthogroup genes without duplicate sequences'''
+    global unique_records #Make unique_records exist outside function
     unique_records = [] #Initialize list of unique records
     checksum_container = [] #Initialize list of unique 
                             #identifiers for each gene
@@ -40,7 +46,9 @@ def duplicate_remover(matching_records):
 
 def file_writer(unique_records):
     '''Write records to file'''
-    SeqIO.write(unique_records, 'filtered_orthofinder.fasta', 'fasta')
+    SeqIO.write(unique_records, orthogroup_name + '_filtered.fasta', 'fasta')
 
 if __name__ == '__main__':
     filter_by_id(orthogroup_file, ids_1kp_file)
+    duplicate_remover(matching_records)
+    file_writer(unique_records)
