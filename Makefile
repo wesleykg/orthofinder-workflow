4c4600410b1980ce6603390c448ecd979ccdc050
wesleykg <wesley.gerelle@gmail.com>
2016-03-07 16:37:45 -0800

    alignment script works with makefile

--- Makefile
+++ Makefile
@@ -1,14 +1,14 @@
-all: $(patsubst %.fna, %_filtered.fasta, $(wildcard *.fna))
+all: $(patsubst %.fna, %_aligned.fasta, $(wildcard *.fna))
 
 clean:
-	rm -f *_filtered.fasta
+	rm -f *_filtered.fasta *_aligned.fasta
 
 %_filtered.fasta: %.fna wanted_species.txt
 	python filter.py $^
 
 %_aligned.fasta : %_filtered.fasta
 	python align.py $^
-	aliview $@
+	nohup aliview $@ > /dev/null 2>&1 &
 
 
 .PHONY: all clean
--- align.py
+++ align.py
@@ -1,5 +1,16 @@
-##Align unique records using Muscle
+'''Usage: align.py <orthogroup_filtered>'''
+
+import os
+from docopt import docopt
 from Bio.Align.Applications import MuscleCommandline
 
-muscle_align = MuscleCommandline(input = 'filtered_orthofinder.fasta', out = 'orthofinder_aligned.fasta')
-muscle_align()
+cmdln_args = docopt(__doc__) #Creates a dictionary of command-line arguments
+
+unaligned_orthogroup = cmdln_args.get('<orthogroup_filtered>')
+orthogroup_name = os.path.splitext(unaligned_orthogroup)[0]
+orthogroup_name = orthogroup_name.rstrip('_filtered')
+
+muscle_align = MuscleCommandline(input = unaligned_orthogroup, 
+                                 out = orthogroup_name + '_aligned.fasta')
+if __name__ == '__main__':
+    muscle_align()
