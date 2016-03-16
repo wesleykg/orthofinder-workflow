71c549ca0618c88d97690bcde6b90467babdf658
wesleykg <wesley.gerelle@gmail.com>
2016-03-16 13:01:41 -0700

    added loops; not working yet

--- retrieve.sh
+++ retrieve.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 prefix="http://onekp.westgrid.ca/1kp-data/"
-fullName=$(cat wanted_species.txt)
-assembly="/assembly/"
-ID=$(awk -F '-' '{print $1}' wanted_species.txt)
+middle="/assembly/"
 suffix="-SOAPdenovo-Trans-Transrate-stats.tsv.gz"
 
-# Need to use a loop instead to construct URL
-URL=$prefix$fullName$assembly$ID$suffix
-echo $URL
+for i in $( cat wanted_species.txt ); do
+	for j in $( awk -F '-' '{print $1}' wanted_species.txt ); do
+			echo $prefix$i$middle$j$suffix
+		done
+	done
