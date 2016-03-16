7f3ef35d0a751fe557ec77401516103072f59e6b
wesleykg <wesley.gerelle@gmail.com>
2016-03-16 11:50:16 -0700

    updated to take ID-Genus_species-#_species_combined form; changed to block comments

--- filter.py
+++ filter.py
@@ -1,40 +1,39 @@
 '''Usage: filter.py <orthogroup> <wanted_species>'''
 
-from docopt import docopt #For command-line arguments
-import os #For creating output filename from input
-from Bio import SeqIO #For reading orthgroup data
+from docopt import docopt # For command-line arguments
+import os # For creating output filename from input
+from Bio import SeqIO # For reading orthgroup data
 
-cmdln_args = docopt(__doc__) #Creates a dictionary of command-line arguments
+cmdln_args = docopt(__doc__) # Creates a dictionary of command-line arguments
 
-orthogroup_file = cmdln_args.get('<orthogroup>')    #The file produced 
-                                                    #by orthofinder
-ids_1kp_file = cmdln_args.get('<wanted_species>')   #The file containing 
-                                                    #wanted 1kp IDs
+orthogroup_file = cmdln_args.get('<orthogroup>')
+wanted_species_file = cmdln_args.get('<wanted_species>')
 
-orthogroup_name = os.path.splitext(orthogroup_file)[0] #Drop the filename
-
-def filter_by_id(orthgroup_file, ids_1kp_file):
+def filter_by_id(orthgroup_file, wanted_species_file):
     '''Filter out transcripts in orthgroup data by species ID'''
     
-    ##Read in list of 1kp IDs of wanted species
-    wanted_ids = [] #Initialize list of IDs
+    # Read in the wanted_species_file as a list of lines. Loop through each
+    # line, splitting it into 0:ID, 1:Genus_species, 2:#-samples-combined and
+    # retain the ID in the list wanted_ids.
+    wanted_ids = []
+    with open(wanted_species_file, 'r') as species_file:
+        wanted_species = species_file.readlines()
+        for line in wanted_species:
+            ID = line.split('-')[0]
+            wanted_ids.append(ID)
     
-    with open(ids_1kp_file, 'r') as ids: #Open the file for reading
-        wanted_ids_temp = ids.readlines() #Read in the file with newline chars
-        for ID in wanted_ids_temp: #Loops through each ID
-            wanted_ids.append(ID.rstrip()) #Remove newline chars
+    # Loop through each wanted ID and if it matches with an ID in the
+    # orthogroup, add that sequence record to the list matching_records
+    matching_records = []
+    for record in SeqIO.parse(orthogroup_file, 'fasta'): 
+        for ID in wanted_ids: 
+            if ID in record.id:
+                matching_records.append(record)
     
-    ##Retrieve records in orthgroup of wanted species only
-    matching_records = [] #Initialize list of records
-    
-    for record in SeqIO.parse(orthogroup_file, 'fasta'): #Open the file
-        for ID in wanted_ids: #Loop through each wanted species ID
-            if ID in record.id: #The '.id' method returns the names of records
-                matching_records.append(record) #Add record to list
-    
-    SeqIO.write(matching_records, 
+    orthogroup_name = os.path.splitext(orthogroup_file)[0] # Drop the filename
+    SeqIO.write(matching_records,
                 orthogroup_name + '_filtered.fasta', 
                 'fasta')
       
 if __name__ == '__main__':
-    filter_by_id(orthogroup_file, ids_1kp_file)
+    filter_by_id(orthogroup_file, wanted_species_file)
