f2fa0c62e764d108a3aec054f666b65bce030e8b
wesleykg <wesley.gerelle@gmail.com>
2016-03-17 13:14:26 -0700

    switched to wget; working

--- download.sh
+++ download.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-curl -u 1kp-data:1kp-rna1 -O "http://onekp.westgrid.ca/1kp-data/SGTW-Ginkgo_biloba/assembly/SGTW-SOAPdenovo-Trans-Transrate-stats.tsv.gz"
+wget --user=1kp-data --password=1kp-rna1 -i wanted_TRS_URLs.txt
+gzip -d *.gz
