dcdec084c09e0466bea39696e28ff6611040fd11
wesleykg <wesley.gerelle@gmail.com>
2016-03-17 10:47:54 -0700

    ditching bash

--- retrieve.sh
+++ retrieve.sh
@@ -4,8 +4,4 @@ prefix="http://onekp.westgrid.ca/1kp-data/"
 middle="/assembly/"
 suffix="-SOAPdenovo-Trans-Transrate-stats.tsv.gz"
 
-for i in $( cat wanted_species.txt ); do
-	for j in $( awk -F '-' '{print $1}' wanted_species.txt ); do
-			echo $prefix$i$middle$j$suffix
-		done
-	done
+#switching to python
