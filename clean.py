3a8bdda6c7c187fd72f88b5a2098529af5a3d83f
wesleykg <wesley.gerelle@gmail.com>
2016-03-17 13:49:47 -0700

    starting on TRS cleaner

--- clean.py
+++ clean.py
@@ -1,4 +1,4 @@
-'''Usage: clean.py <orthogroup_filtered>'''
+'''Usage: clean.py <orthogroup_filtered> <TRS_file>'''
 
 from docopt import docopt #For command-line arguments
 import os #For creating output filename from input
@@ -8,11 +8,13 @@ from Bio.SeqUtils.CheckSum import seguid #For identifying unique sequences
 cmdln_args = docopt(__doc__) #Creates a dictionary of command-line arguments
 
 uncleaned_orthogroup = cmdln_args.get('<orthogroup_filtered>')
-orthogroup_name = os.path.splitext(uncleaned_orthogroup)[0]
-orthogroup_name = orthogroup_name.rstrip('_filtered')
+transrate_stats_file = cmdln_args.get('<TRS_file>')
+
+
 
 def duplicate_remover(uncleaned_orthogroup):
     '''Return a list of orthogroup genes without duplicate sequences'''
+    global unique_records
     unique_records = [] #Initialize list of unique records
     checksum_container = [] #Initialize list of unique 
                             #identifiers for each gene
@@ -23,8 +25,12 @@ def duplicate_remover(uncleaned_orthogroup):
             checksum_container.append(checksum) #Add unique identifier to list
             unique_records.append(record) #Add record to list
     
-    SeqIO.write(unique_records, 
-                orthogroup_name + '_cleaned.fasta', 
+    return unique_records
+
+orthogroup_name = os.path.splitext(uncleaned_orthogroup)[0]
+orthogroup_name = orthogroup_name.rstrip('_filtered')
+
+SeqIO.write(unique_records, orthogroup_name + '_cleaned.fasta', 
                 'fasta')
 
 if __name__ == '__main__':
