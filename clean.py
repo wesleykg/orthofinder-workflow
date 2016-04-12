d8b5adb00baa5b6c4a43fec4a20f43e558f49d25
wesleykg <wesley.gerelle@gmail.com>
2016-04-12 14:29:09 -0700

    removed function due to bug

--- clean.py
+++ clean.py
@@ -1,4 +1,4 @@
-'''Usage: clean.py <orthogroup_filtered> <TRS_file>'''
+'''Usage: clean.py <orthogroup_filtered>''' #<TRS_file>'''
 
 from docopt import docopt #For command-line arguments
 import os #For creating output filename from input
@@ -8,30 +8,21 @@ from Bio.SeqUtils.CheckSum import seguid #For identifying unique sequences
 cmdln_args = docopt(__doc__) #Creates a dictionary of command-line arguments
 
 uncleaned_orthogroup = cmdln_args.get('<orthogroup_filtered>')
-transrate_stats_file = cmdln_args.get('<TRS_file>')
+#transrate_stats_file = cmdln_args.get('<TRS_file>')
 
-
-
-def duplicate_remover(uncleaned_orthogroup):
-    '''Return a list of orthogroup genes without duplicate sequences'''
-    global unique_records
-    unique_records = [] #Initialize list of unique records
-    checksum_container = [] #Initialize list of unique 
-                            #identifiers for each gene
-    for record in SeqIO.parse(uncleaned_orthogroup, 'fasta'):
-        checksum = seguid(record.seq) #Create a unique identifier for only
-                                      #the sequence of the record
-        if checksum not in checksum_container: #Duplicate seqeunce can't pass
-            checksum_container.append(checksum) #Add unique identifier to list
-            unique_records.append(record) #Add record to list
-    
-    return unique_records
+'''Return a list of orthogroup genes without duplicate sequences'''
+unique_records = [] #Initialize list of unique records
+checksum_container = [] #Initialize list of unique 
+                        #identifiers for each gene
+for record in SeqIO.parse(uncleaned_orthogroup, 'fasta'):
+    checksum = seguid(record.seq) #Create a unique identifier for only
+                                  #the sequence of the record
+    if checksum not in checksum_container: #Duplicate seqeunce can't pass
+        checksum_container.append(checksum) #Add unique identifier to list
+        unique_records.append(record) #Add record to list
 
 orthogroup_name = os.path.splitext(uncleaned_orthogroup)[0]
 orthogroup_name = orthogroup_name.rstrip('_filtered')
 
 SeqIO.write(unique_records, orthogroup_name + '_cleaned.fasta', 
                 'fasta')
-
-if __name__ == '__main__':
-    duplicate_remover(uncleaned_orthogroup)
