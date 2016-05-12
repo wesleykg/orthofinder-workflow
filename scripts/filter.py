720515ad7a792bdb7fe4ad6931691136d14369f0
wesleykg <wesley.gerelle@gmail.com>
2016-05-12 09:28:58 -0700

    removed function

--- scripts/filter.py
+++ scripts/filter.py
@@ -9,32 +9,26 @@ cmdln_args = docopt(__doc__) # Creates a dictionary of command-line arguments
 orthogroup_file = cmdln_args.get('<orthogroup>')
 wanted_species_file = cmdln_args.get('<wanted_species>')
 
-def filter_by_id(orthgroup_file, wanted_species_file):
-    '''Filter out transcripts in orthgroup data by species ID'''
-    
-    # Read in the wanted_species_file as a list of lines. Loop through each
-    # line, splitting it into 0:ID, 1:Genus_species, 2:#-samples-combined and
-    # retain the ID in the list wanted_ids.
-    wanted_ids = []
-    with open(wanted_species_file, 'r') as species_file:
-        wanted_species = species_file.readlines()
-        for line in wanted_species:
-            ID = line.split('-')[0]
-            wanted_ids.append(ID)
-    
-    # Loop through each wanted ID and if it matches with an ID in the
-    # orthogroup, add that sequence record to the list matching_records
-    matching_records = []
-    for record in SeqIO.parse(orthogroup_file, 'fasta'): 
-        for ID in wanted_ids: 
-            if ID in record.id:
-                matching_records.append(record)
-    
-    # Write matching_records to file using the original filename and appending
-    # _filtered.fasta to the end
-    orthogroup_name = os.path.splitext(orthogroup_file)[0]
-    SeqIO.write(matching_records, orthogroup_name + '_filtered.fasta', 
-                format = 'fasta')
-      
-if __name__ == '__main__':
-    filter_by_id(orthogroup_file, wanted_species_file)
+#Read in the wanted_species_file as a list of lines. Loop through each line, 
+#splitting it into 0:ID, 1:Genus_species, 2:#-samples-combined and retain the 
+#ID in the list wanted_ids.
+wanted_ids = []
+with open(wanted_species_file, 'r') as species_file:
+    wanted_species = species_file.readlines()
+    for line in wanted_species:
+        ID = line.split('-')[0]
+        wanted_ids.append(ID)
+
+#Loop through each wanted ID and if it matches with an ID in the orthogroup, 
+#add that sequence record to the list matching_records
+matching_records = []
+for record in SeqIO.parse(orthogroup_file, 'fasta'): 
+    for ID in wanted_ids: 
+        if ID in record.id:
+            matching_records.append(record)
+
+#Write matching_records to file using the original filename and appending
+#_filtered.fasta to the end
+orthogroup_name = os.path.splitext(orthogroup_file)[0]
+SeqIO.write(matching_records, orthogroup_name + '_filtered.fasta',
+            format = 'fasta')
