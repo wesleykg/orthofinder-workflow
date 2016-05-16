e992e9909894a930eae052149b13a20b58f7f943
wesleykg <wesley.gerelle@gmail.com>
2016-05-16 15:47:50 -0700

    token now retrieved and supplied in-script

--- scripts/download.py
+++ scripts/download.py
@@ -1,25 +1,26 @@
-'''Usage: download.py <wanted_accessions> <token_file>'''
+'''Usage: download.py <wanted_accessions>'''
 
-#from docopt import docopt
+from docopt import docopt
 from zipfile import ZipFile
 from StringIO import StringIO
-import requests
+import requests, re
 from requests.auth import HTTPDigestAuth
 
-#cmdln_args = docopt(__doc__) # Creates a dictionary of command-line arguments
+cmdln_args = docopt(__doc__) # Creates a dictionary of command-line arguments
 
-wanted_accessions = '../data/wanted_accessions.txt' # cmdln_args.get('<wanted_accessions>')
-token_file = '../data/1kp_token.txt' # cmdln_args.get('<token_file>')
+wanted_accessions = cmdln_args.get('<wanted_accessions>')
 file_format = 'zip'
 
-base_url = 'http://iptol-api.iplantcollaborative.org/onekp/v1/login'
+base_url = 'http://iptol-api.iplantcollaborative.org/onekp/v1/'
 
-with open(token_file, 'r') as token:
-    token_1kp = token.read()
+token = requests.get(base_url + 'login',
+                     auth = HTTPDigestAuth('1kp-data', '1kp-rna1'), 
+                     allow_redirects = False)
 
-token = requests.get(base_url, auth=HTTPDigestAuth('1kp-data', '1kp-rna1'))
+token_pattern = re.compile('[0-9]{13}_[A-z0-9]{16}')
 
-print token.text
+token_1kp = re.search(token_pattern, token.text)
+token_1kp = token_1kp.group()
 
 with open(wanted_accessions, 'r') as accessions:
     accessions = accessions.readlines()
@@ -38,4 +39,5 @@ if orthogroup_file.status_code == requests.codes.ok:
     orthogroup_file = ZipFile(StringIO(orthogroup_file.content))
     orthogroup_file.extractall()
 else:
+    print URL
     orthogroup_file.raise_for_status()
