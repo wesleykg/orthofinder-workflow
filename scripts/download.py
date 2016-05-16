bf087cf8dca1308d8e30ee71b19ac26eb1f4cb25
wesleykg <wesley.gerelle@gmail.com>
2016-05-16 13:33:21 -0700

    working on token

--- scripts/download.py
+++ scripts/download.py
@@ -1,36 +1,41 @@
-'''Usage: download.py <wanted_accesions> <token_file>'''
+'''Usage: download.py <wanted_accessions> <token_file>'''
 
-from docopt import docopt
+#from docopt import docopt
 from zipfile import ZipFile
 from StringIO import StringIO
 import requests
+from requests.auth import HTTPDigestAuth
 
-cmdln_args = docopt(__doc__) # Creates a dictionary of command-line arguments
+#cmdln_args = docopt(__doc__) # Creates a dictionary of command-line arguments
 
-wanted_accesions = cmdln_args.get('<wanted_accesions>')
-token_file = cmdln_args.get('<token_file>')
+wanted_accessions = '../data/wanted_accessions.txt' # cmdln_args.get('<wanted_accessions>')
+token_file = '../data/1kp_token.txt' # cmdln_args.get('<token_file>')
 file_format = 'zip'
 
-base_url = 'http://iptol-api.iplantcollaborative.org/onekp/v1/orthogroups?'
+base_url = 'http://iptol-api.iplantcollaborative.org/onekp/v1/login'
 
 with open(token_file, 'r') as token:
     token_1kp = token.read()
 
-with open(wanted_accesions, 'r') as accesions:
-    accessions = accesions.readlines()
-    accesion_ID_list = []
+token = requests.get(base_url, auth=HTTPDigestAuth('1kp-data', '1kp-rna1'))
+
+print token.text
+
+with open(wanted_accessions, 'r') as accessions:
+    accessions = accessions.readlines()
+    accession_ID_list = []
     for line in accessions:
         line = line.rstrip()
-        accesion_ID_list.append(line)
+        accession_ID_list.append(line)
     seperator = '+'
-    gene_IDs = seperator.join(accesion_ID_list)
+    gene_IDs = seperator.join(accession_ID_list)
     
-URL = (base_url + 'accession=' + gene_IDs + '&token=' + token_1kp + 
-        '&format=' + file_format)
+URL = (base_url + 'orthogroups?' + 'accession=' + gene_IDs + '&token='
+        + token_1kp + '&format=' + file_format)
 
 orthogroup_file = requests.get(URL)
 if orthogroup_file.status_code == requests.codes.ok:
     orthogroup_file = ZipFile(StringIO(orthogroup_file.content))
     orthogroup_file.extractall()
 else:
-    print 'Status code:', orthogroup_file.status_code
+    orthogroup_file.raise_for_status()
