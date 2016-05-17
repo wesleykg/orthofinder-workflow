f2edbd38adcb62f0b4c23ab3f586008c2c9dfc93
wesleykg <wesley.gerelle@gmail.com>
2016-05-17 15:01:27 -0700

    fiddling with stuff

--- scripts/0_download.py
+++ scripts/0_download.py
@@ -18,27 +18,28 @@ orthogroup_url = base_url + 'orthogroups'
 
 #Connect to the server and obtain an authentication token. Setting ridirection
 #to false stops Requests from redirecting us to the web GUI page
-token = requests.get(token_url, auth = HTTPDigestAuth('1kp-data', '1kp-rna1'),
-                     allow_redirects = False)
+auth_token = requests.get(token_url, 
+                          auth=HTTPDigestAuth('1kp-data', '1kp-rna1'), 
+                            allow_redirects = False)
 
 #Use regex to find the token in the returned message from the server
 token_pattern = re.compile('[0-9]{13}_[A-z0-9]{16}')
-token_1kp = re.search(token_pattern, token.text)
-token_1kp = token_1kp.group() #Method to actually read the matched regex token
+auth_token = re.search(token_pattern, auth_token.text)
+auth_token = auth_token.group() #Method to read the matched regex token
 
 #Open the wanted_accessions file and read in each accession into accession_ids
 accession_IDs = []
 with open(wanted_accessions, 'r') as accessions:
-    wanted_accesions = accessions.readlines()
-    for line in wanted_accessions:
+    accessions_list = accessions.readlines()
+    for line in accessions_list:
         line = line.rstrip()
         accession_IDs.append(line)
     gene_IDs = '+'.join(accession_IDs)
     
-URL = (base_url + 'orthogroups?' + 'accession=' + gene_IDs + '&token='
-        + token_1kp + '&format=' + file_format)
+URL = (orthogroup_url + '?accession=' + gene_IDs + '&token='
+        + auth_token + '&format=' + file_format)
 
-orthogroup_file = requests.get(orthogroup_url)
+orthogroup_file = requests.get(URL)
 if orthogroup_file.status_code == requests.codes.ok:
     orthogroup_file = ZipFile(StringIO(orthogroup_file.content))
     orthogroup_file.extractall()
