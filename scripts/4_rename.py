2337e628625c7a9958cdcd7faf93717184033bad
wesleykg <wesley.gerelle@gmail.com>
2016-09-26 11:16:57 -0700

    interactively renames alignments

--- scripts/4_rename.py
+++ scripts/4_rename.py
@@ -1,8 +1,10 @@
-'''Usage: 4_rename.py <file> <names_file>'''
+'''Usage: 4_rename.py <fasta_alignment> <names_file>'''
 
 # Modules
 import pandas
 import os
+from Bio import SeqIO
+
 
 # Check if running interactively in an iPython console, or in a script
 # from the command-line
@@ -16,11 +18,27 @@ def in_ipython():
 if in_ipython() is False:
     from docopt import docopt  # Command-line argument handler
     cmdln_args = docopt(__doc__)
-    in_file = cmdln_args.get('<fasta_file>')
-    names_file = cmdln_args.get('<names_file>')
+    in_alignment = cmdln_args.get('<fasta_alignment>')
+    accessions_file = cmdln_args.get('<names_file>')
 # Run interatively in an iPython console
 if in_ipython() is True:
-    in_file = ''
-    names_file = '../kegg_pathway_photosynthesis'
+    in_alignment = '../data/ATCG00540_10485_aligned.fasta'
+    accessions_file = '../data/PHYPA_ARATH_pt-gene-accessions.tsv'
 
-pandas.read_csv()
+alignment = SeqIO.parse(in_alignment, format='fasta')
+
+table_header = 'gene', 'accession'
+
+gene_accessions = pandas.read_csv(accessions_file, sep=',', names=table_header)
+
+alignment_filename = os.path.split(in_alignment)[1]
+alignment_file_accession_name = alignment_filename.partition('_')[0]
+alignment_file_tail_name = alignment_filename.partition('_')[2]
+
+fasta_file_gene_name = \
+    gene_accessions[gene_accessions.accession == alignment_file_accession_name]
+fasta_file_gene_name = fasta_file_gene_name.iloc[0]['gene']
+
+fasta_file_outname = fasta_file_gene_name + '_' + alignment_file_tail_name
+
+SeqIO.write(alignment, handle=fasta_file_outname, format='fasta')
